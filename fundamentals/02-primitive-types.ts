4c01c8689bc26486982f704b84ba1630a740aeb2
Your Name <you@example.com>
2022-03-03 14:27:35 +0100

    Typescript: The Ultimate Bootcamp

---
+++ fundamentals/02-primitive-types.ts
@@ -0,0 +1,27 @@
+
+// primitive types: number
+const lessonsCount = 10;
+
+const total = lessonsCount + 10;
+
+console.log("total =", total);
+
+// primitive types: string
+let title = "Typescript Bootcamp";
+
+let subtitle = "Learn the language fundamentals, build practical projects";
+
+const fullTitle = `Full title: ${title}: ${subtitle}`;
+
+console.log(`Full title: ${fullTitle}`);
+
+// primitive types: boolean
+const published = true;
+
+if (published) {
+    console.log("The course is published.");
+}
+
+
+
+
