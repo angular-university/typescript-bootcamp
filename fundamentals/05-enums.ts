ca94e3c76804e1390bb64dce42de4f31eba57f1e
Your Name <you@example.com>
2022-03-11 16:26:50 +0100

    Typescript: The Ultimate Bootcamp

---
+++ fundamentals/05-enums.ts
@@ -0,0 +1,15 @@
+
+enum CourseType {
+    FREE = "FREE",
+    PREMIUM = "PREMIUM",
+    PRIVATE = "PRIVATE",
+    HIDDEN ="HIDDEN"
+}
+
+const course = {
+    title: "Typescript Bootcamp",
+    type: CourseType.HIDDEN
+};
+
+console.log(course);
+
