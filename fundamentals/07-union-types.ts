8e4774874a7ef9691ce168a27baa66c6149e649f
Your Name <you@example.com>
2022-03-18 15:09:18 +0100

    Typescript: The Ultimate Bootcamp

---
+++ fundamentals/07-union-types.ts
@@ -0,0 +1,14 @@
+
+
+
+let uniqueIdentifier: number | string = 1000;
+
+uniqueIdentifier = "201e72bb-49e3-40ef-8331-b7f3e7d947f8";
+
+const keys: (number | string) [] = [1000, "Hello"];
+
+let courseId: number | null = 1000;
+
+courseId = null;
+
+
