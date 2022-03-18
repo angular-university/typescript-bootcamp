21f6c042a7f0af1464cf12f1e43d17adb5d7389d
Your Name <you@example.com>
2022-03-18 14:18:49 +0100

    Typescript: The Ultimate Bootcamp

--- fundamentals/06-any-type.ts
+++ fundamentals/06-any-type.ts
@@ -1,7 +1,12 @@
 
-
-
-
 let lessonsCount:any = 10;
 
 let numbers : any[] = [10, 20, "Hello", true];
+
+printCourse( "Typescript Bootcamp", 10);
+
+function printCourse(title:string, lessonsCount:number) {
+
+    console.log(`Title: ${title}, lessons count: ${lessonsCount}`);
+
+}
