de8ded4316953c3a7ea97e8a52a991bf38be1aea
Your Name <you@example.com>
2022-03-24 16:09:31 +0100

    Typescript: The Ultimate Bootcamp

---
+++ fundamentals/08-literal-types.ts
@@ -0,0 +1,13 @@
+
+
+
+const title = "Typescript Bootcamp";
+
+const lessonsCount = 10;
+
+let pageSize: 10 | 15 | 20 = 10;
+
+let courseStatus: "draft" | "published" | "unpublished" |
+    "archived" = "draft";
+
+courseStatus = "published";
