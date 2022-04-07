989294f16be79e3ad6730e7b104a1bbbca061464
Your Name <you@example.com>
2022-04-07 15:28:16 +0200

    Typescript: The Ultimate Bootcamp

---
+++ fundamentals/12-modules-exports.ts
@@ -0,0 +1,21 @@
+
+export type Course = {
+    readonly title:string,
+    subtitle:string,
+    lessonsCount?:number
+};
+
+export const PAGE_SIZE = 100;
+
+const pageSize = PAGE_SIZE;
+
+export const COURSE = {
+    title: "Typescript Bootcamp",
+    subtitle: "Learn the language fundamentals, build practical projects",
+    lessonsCount: 10
+};
+
+
+
+
+
---
+++ fundamentals/12-modules-imports.ts
@@ -0,0 +1,4 @@
+
+import {PAGE_SIZE, COURSE, Course} from "./12-modules-exports";
+
+const pageSize = PAGE_SIZE;
