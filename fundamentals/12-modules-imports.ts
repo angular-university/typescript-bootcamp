ddcfdb0e559457d99e9083632ca0db4acd86e989
Your Name <you@example.com>
2022-04-22 13:21:04 +0200

    Typescript: The Ultimate Bootcamp

--- fundamentals/12-modules-imports.ts
+++ fundamentals/12-modules-imports.ts
@@ -7,3 +7,8 @@ const pageSize = PAGE_SIZE;
 import {Course, loadAllCourses, saveCourse}
     from "./14-module-reexports";
 
+import printCourse from "./15-default-exports";
+
+import * as constants from "./15-default-exports";
+
+printCourse({});
---
+++ fundamentals/15-default-exports.ts
@@ -0,0 +1,15 @@
+
+
+export const COURSE_TOTAL = 20;
+
+export const TYPESCRIPT_COURSE = {
+    title: "Typescript Bootcamp",
+    subtitle: "Learn the language fundamentals, build practical projects",
+    lessonsCount: 10
+};
+
+export default function printCourse(course) {
+    console.log(`The course title is ${course.title}`);
+}
+
+
