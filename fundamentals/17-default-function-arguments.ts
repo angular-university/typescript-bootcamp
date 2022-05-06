db2ea7f57cb1c6a7ae5c48f857e452d4cd3df733
Your Name <you@example.com>
2022-05-06 13:21:40 +0200

    Typescript: The Ultimate Bootcamp

---
+++ fundamentals/17-default-function-arguments.ts
@@ -0,0 +1,22 @@
+
+
+
+
+
+function printCourse(title = "TITLE", subtitle = "SUBTITLE", lessonsCount = 0) {
+
+    console.log(`Title: ${title}, Subtitle: ${subtitle} lessons count: ${lessonsCount}`);
+
+}
+
+printCourse(
+    "Typescript Bootcamp",
+    "Learn the language fundamentals, build practical projects",
+    10);
+
+printCourse(
+    "Typescript Bootcamp",
+    "Learn the language fundamentals, build practical projects"
+);
+
+printCourse();
