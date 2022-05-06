336113f336e190aa922c4b09345e3055c261ab0e
Your Name <you@example.com>
2022-05-06 15:24:13 +0200

    Typescript: The Ultimate Bootcamp

---
+++ fundamentals/20-array-spread-destructuring.ts
@@ -0,0 +1,10 @@
+
+const numbers = [1, 2 ,3];
+
+const moreNumbers = [...numbers, 4, 5, 6];
+
+console.log(moreNumbers);
+
+const [first, second, third] = moreNumbers;
+
+console.log(first, second, third);
