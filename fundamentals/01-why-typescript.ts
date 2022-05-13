82cd335e1c63052663664df9f85a0f10d47494de
jhades <jhades.dev@gmail.com>
2022-05-13 12:38:22 +0200

    Typescript Bootcamp

--- fundamentals/01-why-typescript.ts
+++ fundamentals/01-why-typescript.ts
@@ -1,6 +1,8 @@
 
 const courseName = "Typescript Bootcamp";
 
+debugger;
+
 if (courseName) {
 
     const subtitle = "Learn the language fundamentals, build practical projects";
@@ -9,6 +11,8 @@ if (courseName) {
 }
 
 function printCourseName(name :string) {
+
+    debugger;
 
     console.log("The course name is " + name.toUpperCase());
 
