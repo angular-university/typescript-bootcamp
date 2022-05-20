45e67b37352221b8c8bf79e825bcc5fc4ace2818
jhades <jhades.dev@gmail.com>
2022-05-20 12:47:05 +0200

    Typescript Bootcamp

--- fundamentals/23-introduction-to-functions.ts
+++ fundamentals/23-introduction-to-functions.ts
@@ -6,4 +6,24 @@ interface Course {
     lessonsCount:number;
 }
 
+function createCourse(title:string, subtitle:string,
+                      lessonsCount:number) :Course {
+
+    console.log(` Creating course with Title: ${title}, 
+    Subtitle: ${subtitle} lessons count: ${lessonsCount}`);
+
+    return {
+        title,
+        subtitle,
+        lessonsCount
+    };
+}
+
+const result = createCourse("Typescript Bootcamp", "Learn the language fundamentals", 100);
+
+console.log(typeof createCourse);
+
+
+
+
 
