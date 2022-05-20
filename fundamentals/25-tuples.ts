c7196c49aa10b59abc72012db99240d63c13ae09
jhades <jhades.dev@gmail.com>
2022-05-20 13:54:08 +0200

    Typescript Bootcamp

--- fundamentals/25-tuples.ts
+++ fundamentals/25-tuples.ts
@@ -2,18 +2,19 @@
 interface Course {
     title:string;
     subtitle: string;
+    lessonsCount:number;
 }
 
-function createCourse(title:string, subtitle:string) {
+type CourseRecord = [string, string, number];
+
+const courseRecord: CourseRecord =
+    ["Typescript Bootcamp","Learn the language fundamentals", 100];
+
+function createCourse(title:string, subtitle:string): CourseRecord {
 
     console.log(` Creating course with Title: ${title}, Subtitle: ${subtitle} `);
 
-    const course = {
-        title,
-        subtitle
-    } as Course;
-
-    return course;
+    return [title, subtitle, 100];
 }
 
 
