b44048603bb9ce6157b76305a4547a7234f9d847
jhades <jhades.dev@gmail.com>
2022-06-23 15:35:56 +0200

    Typescript Bootcamp

--- fundamentals/src/12-modules-imports.ts
+++ fundamentals/src/12-modules-imports.ts
@@ -3,6 +3,7 @@ import {PAGE_SIZE, COURSE} from "./12-modules-exports";
 
 const pageSize = PAGE_SIZE;
 
+import {HELLO_WORLD} from './32-plain-javascript'
 
 import {Course, loadAllCourses, saveCourse}
     from "14-module-reexports";
