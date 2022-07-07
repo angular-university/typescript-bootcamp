8335f73845a4bfbdeb534dd33a1a93843e43c7ed
jhades <jhades.dev@gmail.com>
2022-07-07 14:05:48 +0200

    Typescript Bootcamp

---
+++ generics/04-generic-functions.ts
@@ -0,0 +1,38 @@
+
+interface Course {
+    title:string;
+    subtitle:string;
+    lessonsCount: number;
+}
+
+export function freezeCourse(course:Course): Readonly<Course> {
+    return Object.freeze(course);
+}
+
+function freezeLesson(lesson:Lesson): Readonly<Lesson> {
+    return Object.freeze(lesson);
+}
+
+function freeze<T>(input: T): Readonly<T> {
+    return Object.freeze(input);
+}
+
+const course: Course = {
+    title: "Typescript Bootcamp",
+    subtitle: "Learn the language, build practical projects",
+    lessonsCount: 100
+}
+
+const frozenCourse = freeze(course);
+
+//frozenCourse.title = "";
+
+interface Lesson {
+    title:string;
+    seqNo:number;
+}
+
+const frozenLesson = freeze({
+    title: "Lesson Title",
+    seqNo: 10
+})
