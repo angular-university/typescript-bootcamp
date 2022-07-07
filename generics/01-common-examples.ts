f71c5241462a92aa704c2fe5bef6dc15d4470851
jhades <jhades.dev@gmail.com>
2022-07-07 10:48:35 +0200

    Typescript Bootcamp

--- generics/01-common-examples.ts
+++ generics/01-common-examples.ts
@@ -3,5 +3,13 @@ const numbers = new Array<number>();
 
 numbers.push(10);
 
+const promise = new Promise<string>((resolve, reject) => {
+
+    resolve("Hello World");
+
+});
+
+promise.then(val => {
 
 
+})
