7ab7b439d4fe21b1c31e6b2f075179bd0d26b9f1
jhades <jhades.dev@gmail.com>
2022-08-30 13:36:05 +0200

    Preparation for REST API

--- angular-frontend/src/app/course-dialog/course-dialog.component.ts
+++ angular-frontend/src/app/course-dialog/course-dialog.component.ts
@@ -2,7 +2,6 @@ import {Component, Inject, OnInit, ViewEncapsulation} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialog, MatDialogConfig, MatDialogRef} from '@angular/material/dialog';
 import {Course} from "../model/course";
 import {FormBuilder, Validators, FormGroup} from "@angular/forms";
-import * as moment from 'moment';
 
 @Component({
     selector: 'course-dialog',
