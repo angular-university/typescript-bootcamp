7810a656c9e72a0304d0b7930dd2f010abf62f0f
jhades <jhades.dev@gmail.com>
2022-09-08 14:58:54 +0200

    Typescript Bootcamp

--- rest-api/src/server.ts
+++ rest-api/src/server.ts
@@ -1,3 +1,13 @@
+
+import * as dotenv from "dotenv";
+
+const result = dotenv.config();
+
+if (result.error) {
+    console.log(`Error loading environment variables, aborting.`);
+    process.exit(1);
+}
+
 import * as express from 'express';
 import {root} from "./routes/root";
 import {isInteger} from "./utils";
@@ -8,7 +18,6 @@ const app = express();
 function setupExpress() {
 
     app.route("/").get(root);
-
 
 }
 
