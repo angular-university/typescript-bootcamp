2375cbafc3f28024118244ccded562051d728bcb
jhades <jhades.dev@gmail.com>
2022-09-27 15:36:04 +0200

    Typescript Bootcamp

--- rest-api/src/routes/find-course-by-url.ts
+++ rest-api/src/routes/find-course-by-url.ts
@@ -1,5 +1,8 @@
 import {NextFunction, Request, Response} from "express";
 import {logger} from "../logger";
+import {AppDataSource} from "../data-source";
+import {Course} from "../models/course";
+import {Lesson} from "../models/lesson";
 
 
 export async function findCourseByUrl(
@@ -16,6 +19,31 @@ export async function findCourseByUrl(
             throw `Could not extract the course url from the request.`;
         }
 
+        const course = await AppDataSource
+            .getRepository(Course)
+            .findOneBy({
+                url: courseUrl
+            });
+
+        if (!course) {
+            const message = `Could not find a course with url ${courseUrl}`;
+            logger.error(message);
+            response.status(404).json({message});
+            return;
+        }
+
+        const totalLessons = await AppDataSource
+            .getRepository(Lesson)
+            .createQueryBuilder("lessons")
+            .where("lessons.courseId = :courseId", {
+                courseId: course.id
+            })
+            .getCount()
+
+        response.status(200).json({
+           course,
+           totalLessons
+        });
 
 
     }
