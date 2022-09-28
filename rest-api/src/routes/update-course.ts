79216caf13dd9a86eb1c9356ceaf6b252f5afa99
jhades <jhades.dev@gmail.com>
2022-09-28 17:28:20 +0200

    Typescript Bootcamp

--- rest-api/src/routes/update-course.ts
+++ rest-api/src/routes/update-course.ts
@@ -1,6 +1,14 @@
 import {NextFunction, Request, Response} from "express";
 import {logger} from "../logger";
+import {isInteger} from "../utils";
+import {AppDataSource} from "../data-source";
+import {Course} from "../models/course";
 
+/*
+*
+* curl -X PATCH http://localhost:9000/api/courses/76 -H "Content-Type:application/json" -d '{"title":"Typescript Bootcamp v2"}'
+*
+**/
 
 export async function updateCourse(
     request: Request, response: Response, next:NextFunction) {
@@ -11,6 +19,21 @@ export async function updateCourse(
 
         const courseId = request.params.courseId,
               changes = request.body;
+
+        if (!isInteger(courseId)) {
+            throw `Invalid course id ${courseId}`;
+        }
+
+        await AppDataSource
+            .createQueryBuilder()
+            .update(Course)
+            .set(changes)
+            .where("id = :courseId", {courseId})
+            .execute();
+
+        response.status(200).json({
+            message: `Course ${courseId} was updated successfully.`
+        });
 
     }
     catch (error) {
